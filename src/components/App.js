60d06b562eacbc8df9c8671c1c9610fa24b39283
hicksey <hicksey14@gmail.com>
2020-12-02 14:24:21 -0700

    add template html, css, app.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,19 @@ import '../css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main class="   " id="  ">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12  ">
+            <div class="container">
+              <div>Add Lessons</div>
+              <div>Search Lessons</div>
+              <div>List Lessons</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
