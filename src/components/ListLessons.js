752e8d954daa7cebda87f9872808d611690a85b7
hicksey <hicksey14@gmail.com>
2020-12-17 17:43:00 -0700

    list lessons component aesthetic appearance

--- src/components/ListLessons.js
+++ src/components/ListLessons.js
@@ -9,23 +9,58 @@ class ListLessons extends Component {
     return (
       <div className="lesson-list item-list mb-3">
         {this.props.lessons.map(item => (
-          <div className="board-item col media py-3">
-            <div className="mr-3">
-              <button className="board-delete btn btn-sm btn-danger">X</button>
-            </div>
-
-            <div className="board-info media-body">
-              <div className="board-head d-flex">
-                <span className="board-name">Boarder Name</span>
-                <span className="lsn-date ml-auto">Lesson Date</span>
+          <div class="container board-item">
+            <div class="row">
+              <div class="col-3 col-md-4 brd">
+                <span class="lsn-date">
+                2021-1-9 9:30
+                </span>
               </div>
-              <div className="lsn-length">Lesson Length</div>
+              <div class="col-3 col-md-3 brd">
+                <span class="label-item">
+                Rider:
+                </span>
+                <span class="board-name">
+                Jeremy
+                </span>
+              </div>
+              <div class="col-2 col-md-2 brd lsn-length">
+                Half Day
+              </div>
+              <div class="col-2 col-md-1 brd lsn-price">
+                $50
+              </div>
+              <div class="col-2 col-md-2 brd">
+                <button class="btn btn-sm btn-danger">X</button>
+              </div>
             </div>
-
+            <div class="row">
+              <div class="col-3 col-md-4 brd">
+                <span class="lsn-date">
+                  2021-1-9 9:30
+                </span>
+              </div>
+              <div class="col-3 col-md-3 brd">
+                <span class="label-item">
+                  Rider:
+                </span>
+                <span class="board-name">
+                  Mitch
+                </span>
+              </div>
+              <div class="col-2 col-md-2 brd lsn-length">
+                Half Day
+              </div>
+              <div class="col-2 col-md-1 brd lsn-price">
+                $50
+              </div>
+              <div class="col-2 col-md-2 brd">
+                <button class="btn btn-sm btn-danger">X</button>
+              </div>
+            </div>
           </div>
         ))}
       </div>
-
     );
   }
 }
