b52ce66d83450aa1a28853f52ef5aa71f68ccffc
hicksey one-four <hicksey14@gmail.com>
2021-05-11 10:27:08 -0600

    place snow jumbotron image at top of app

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,17 @@ import '../css/App.css';
 export default function App() {
   return (
     <BrowserRouter>
+      <div className="jumbotron jumbotron-fluid pt-0 pb-0">
+        <div className="container ml-0 pl-0">
+            <img src="https://wallpapercave.com/wp/pmSJfNk.jpg"className="mx-auto d-block" width="1500px" height="900px" alt="Snow falling" />
+        </div>
+      </div>
+
         <main>
           <Switch>
             <Route exact path="/">
-            <Link to="/about/">About Instructors</Link>
-            <Link to="/schedule/">Schedule Now</Link>
+            <Link className="btn btn-info btn-lg" role="button" to="/about/">About Instructors</Link>
+            <Link className="btn btn-info btn-lg" role="button" to="/schedule/">Schedule Now</Link>
             </Route>
             <Route path="/about/">
               <About  />
