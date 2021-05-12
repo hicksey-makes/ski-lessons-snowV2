eefe5682f40e23eca73a067479e9ca8451193e63
hicksey one-four <hicksey14@gmail.com>
2021-05-11 21:25:31 -0600

    app.js homepage with background image and responsive Link buttons

--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,10 @@ export default function App() {
           <Switch>
             <Route className="backdrop" exact path="/">
             <div className="img-snow">
-              <Link className="btn btn-info btn-lg" role="button" to="/about/">About Instructors</Link>
-              <Link className="btn btn-info btn-lg" role="button" to="/schedule/">Schedule Now</Link>
+              <div className="d-flex justify-content-center grow">
+                  <Link className="btn btn-info btn-lg first mr-3 shrink" role="button" to="/about/">About Instructors</Link>
+                  <Link className="btn btn-info btn-lg next ml-3 shrink" role="button" to="/schedule/">See Schedule</Link>
+              </div>
             </div>
             </Route>
 
