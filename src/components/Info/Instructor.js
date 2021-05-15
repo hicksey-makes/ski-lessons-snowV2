f4afcd4c774f8719aecf99ccfdfaad05769edb53
hicksey <hicksey14@gmail.com>
2021-05-15 13:48:50 -0600

    add placeholder image and background on about page

--- src/components/Info/Instructor.js
+++ src/components/Info/Instructor.js
@@ -17,18 +17,19 @@ export default function Instructor() {
   console.log(instructors);
 
   return (
-    <div className="container">
-    <div className="btn-group btn-group-justified" style={{width:100 + "%"}}>
-
+    <div className="container-fluid">
+      <div className="btn-group btn-group-justified" style={{width:100 + "%"}}>
         {instructors.map((instructor) => (
-
             <Link className="btn btn-secondary "  key={instructor.id} to={`instructors/${instructor.id}`}>
               {instructor.name}
             </Link>
-
         ))}
-
-    </div>
+      </div>
+      <div className="bg-info text-center text-light">
+        <h3 className="margin">Find the instructor</h3>
+        <img  src="../images/snowboard_icon_colorpow.png" style={{width:450 + "px"}} alt="snowboarder icon"/>
+        <h3>Right for your desires and abilities.</h3>
+      </div>
     </div>
   );
 }
