fe644094e8234c67c309e30ade16e517956847ba
hicksey <hicksey14@gmail.com>
2021-05-15 19:35:41 -0600

    increase about image padding

--- src/components/Info/Instructor.js
+++ src/components/Info/Instructor.js
@@ -27,7 +27,7 @@ export default function Instructor() {
       </div>
       <div className="bg-dark text-center text-light mb-n2">
         <h3 className="margin pt-4 pb-2">Find the instructor</h3>
-        <img className="img-rounded" src="../images/powder_freshies_a.stock.jpeg" style={{width:450 + "px", height:450 + "px"}} alt="snowboarder icon"/>
+        <img className="img-rounded p-1" src="../images/powder_freshies_a.stock.jpeg" style={{width:450 + "px", height:450 + "px"}} alt="snowboarder icon"/>
         <h3 className="pb-4 pt-2">for your desires and abilities.</h3>
       </div>
     </div>
