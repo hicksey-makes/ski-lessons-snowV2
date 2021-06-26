f4945cfad5a7a07569c1c6cd97083af9922f941a
hicksey <hicksey14@gmail.com>
2021-06-26 00:36:43 -0600

    add cart button to About comp

--- src/components/Info/About.js
+++ src/components/Info/About.js
@@ -7,6 +7,9 @@ import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 export default function About() {
   return (<>
     <Link className="btn blendin mb-3 mt-3 ml-4" style={{width:80 + "px"}} to="/">Home</Link>
+    <div className="d-block ml-auto mt-n5" style={{width: 150 + "px"}}>
+      <Link className="btn btn-warning mb-3" style={{width: 80 + "px"}} to="/cart/">Cart</Link>
+    </div>
     <BrowserRouter>
       <Switch>
         <Route exact path="/about">
