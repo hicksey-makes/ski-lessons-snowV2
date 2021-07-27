5bcecd003992dd122c8fbac64dae13c6e1f16a16
hicksey <hicksey14@gmail.com>
2021-07-26 18:19:45 -0600

    correct spacing of AddLesson comp elements during screen resize

--- src/components/Reserve/AddLessons.js
+++ src/components/Reserve/AddLessons.js
@@ -34,17 +34,17 @@ class AddLessons extends Component {
             </div>
             <div className="form-group form-row mb-md-5 mb-1">
               <label className="col-md-1 ml-md-4 mt-0 mt-md-2 text-md-right" htmlFor="lsnType">Type:</label>
-              <h5 className="col-md-3 mt-0 mt-md-2 font-weight-light font-italic text-primary">
+              <h5 className="col-md-2 mt-0 mt-md-2 font-weight-light font-italic text-primary">
                 {this.props.selectedLsn.lsnType}
               </h5>
-                <div className="col-6 col-md-2 mb-2 mt-4 mt-md-1 ml-1">
-                  <button type="button" className="btn btn-warning add-btn " disabled={this.props.lessonLength === "Full Day"} active={this.props.lessonLength === "Full Day"} href="#" id="lsnLength" value="Full Day" onClick={event => this.props.setPrice(event)}>Full Day (5 hr)</button>
+                <div className="col-6 col-md-2 mb-2 mt-4 mt-md-1 ml-md-0 ml-1">
+                  <button type="button" className="btn btn-warning add-btn ml-md-2 ml-lg-5" disabled={this.props.lessonLength === "Full Day"} active={this.props.lessonLength === "Full Day"} href="#" id="lsnLength" value="Full Day" onClick={event => this.props.setPrice(event)}>Full Day</button>
                 </div>
-                <div className=" col-6 col-md-2 mb-2 mt-md-1 ml-1">
+                <div className="col-8 col-md-3 mb-2 mt-md-1 ml-1">
                   <button type="button" className="btn btn-warning add-btn" disabled={this.props.lessonLength === "Half Day"} active={this.props.lessonLength === "Half Day"} href="#" id="lsnLength" value="Half Day" onClick={(event) => this.props.setPrice2(event)}>Half Day (2.5 hr)</button>
                 </div>
-              <label className="col-md-1 mt-3 mt-md-2 ml-md-4 text-md-right">Price:</label>
-              <div className="col-md-2  mt-md-2 text-dark">
+              <label className="ml-md-2 col-md-1 mt-3 mt-md-2 text-md-right">Price:</label>
+              <div className=" col-md-2  mt-md-2 text-dark">
               <h5 className="font-weight-light font-italic text-primary">
               $ {this.props.selectedLsn.lessonCost}
               </h5>
