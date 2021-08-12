e090c3ce7b59d0c30fb8910e3b7c334e9321d6a9
hicksey <hicksey14@gmail.com>
2021-08-12 15:28:37 -0600

    add margin top/bottom to headline 3's to space from image in Instructor.js

--- src/components/Info/Instructor.js
+++ src/components/Info/Instructor.js
@@ -26,13 +26,13 @@ export default function Instructor() {
         ))}
       </div>
       <div className="bg-dark text-center text-light mb-n2 container-fluid ins-bg grow relative">
-        <div className="ins-content">
-        <h3 className=" pt-4 pb-2 mint-spice">Find the right instructor</h3>
+        <div className="ins-content mt-5 pt-1">
+        <h3 className=" pt-5 pb-2 mb-4 mint-spice">Find the right instructor</h3>
 
           <img className="img-rounded rounded-circle photo-generic p-1" id="photo-gen" src="../images/powder_freshies_a.stock.jpeg"  alt="snowboarder icon"/>
 
 
-        <h3 className="pb-4 pt-2 mint-spice">for your desires and abilities.</h3>
+        <h3 className="pb-4 pt-2 mt-4 mint-spice">for your desires and abilities.</h3>
         </div>
     </div>
     </div>
