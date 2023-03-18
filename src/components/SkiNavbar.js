e59b7c37fc764650a5af69f5f49c651e26eb242e
hicksey <hicksey14@gmail.com>
2023-03-18 17:36:54 -0600

    add class nav-link and exact prop to branding navlink for styling and active behavior, move #branding to same to preserve responsive behavior

--- src/components/SkiNavbar.js
+++ src/components/SkiNavbar.js
@@ -6,18 +6,20 @@ function SkiNavbar() {
 
   return (<>
     <Nav id="navbar" className="justify-content-center flex-column text-center">
-    <NavLink to="/">
-    <Navbar.Brand id="branding" className="">
-      <img
-        src="/images/snowboard_icon75-75.png"
-        width="40"
-        height="40"
-        className=""
-        alt=""
-      />{' '}
-      Butter Boarder
-    </Navbar.Brand>
+
+    <NavLink exact to="/" id="branding" className="nav-link">
+      <Navbar.Brand  className="">
+        <img
+          src="/images/snowboard_icon75-75.png"
+          width="40"
+          height="40"
+          className=""
+          alt=""
+        />{' '}
+        Butter Boarder
+      </Navbar.Brand>
     </NavLink>
+
       <Nav.Item className="topnav">
         <NavLink to="/about/" className="nav-link">About</NavLink>
       </Nav.Item>
