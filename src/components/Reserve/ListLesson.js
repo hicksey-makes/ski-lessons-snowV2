ed5623a5515c12add83d9ee02a0c45f2e51d55be
hicksey <hicksey14@gmail.com>
2023-05-01 16:53:47 -0600

    update spacing utils to bs5 versions on lsn price text in ListLesson comp

--- src/components/Reserve/ListLesson.js
+++ src/components/Reserve/ListLesson.js
@@ -40,10 +40,10 @@ class ListLesson extends Component {
               {this.props.lsns.lsnType}
             </div>
             <div className="col-2 col-md-3 pl-2 pt-md-3 label-item brd text-white text-break pos-rel">
-              <div className="push-r mt-n2 mb-2 mb-md-0 lg-inline ml-1 ml-sm-2 ml-md-3 mr-md-4">
+              <div className="push-r mt-n2 mb-2 mb-md-0 lg-inline ms-1 ms-sm-2">
                 {this.props.lsns.lessonCost}
               </div>
-              <button className="btn btn-sm btn-warning px-sm-2 mt-md-0 mr-n1 ml-sm-1 ml-md-5 px-md-3 pb-md-2 d-block lg-inline cal-pos" id="" onClick={() => {this.props.toggleAdd(); this.props.onLessonSelect(this.props.lsns)}}><FaRegCalendarCheck className="mt-n1"/></button>
+              <button className="btn btn-sm btn-warning px-sm-2 mt-md-0  px-md-3 pb-md-2 mr-n1 ml-sm-1 ms-sm-1 ml-md-5 d-block lg-inline cal-pos" id="" onClick={() => {this.props.toggleAdd(); this.props.onLessonSelect(this.props.lsns)}}><FaRegCalendarCheck className="mt-n1"/></button>
             </div>
 
           </div>
