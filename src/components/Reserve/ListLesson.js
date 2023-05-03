bab6af6bb2ec577f7f708823c49bda2e8b5b9d56
hicksey <hicksey14@gmail.com>
2023-05-02 20:32:57 -0600

    add background-gradient bootstrap utility to ListLesson, rearrange classNames to put utilities after named classes

--- src/components/Reserve/ListLesson.js
+++ src/components/Reserve/ListLesson.js
@@ -16,17 +16,16 @@ class ListLesson extends Component {
 
     return (
         <div className="container board-item" key={this.props.lsns.lsnId}>
-          <div className="row lsn-row bg-dark pb-1 pt-md-3 pb-md-3 pb-lg-4">
-            <div className="col-3 col-md-3 brd pt-md-3  text-white lsn-date pos-rel">
-
-            {`${((new Date(this.props.lsns.lsnTime)).toDateString()).substring(3,10)}`}
-            <div className="time-pos">
-              {`${((new Date(this.props.lsns.lsnTime)).toTimeString()).substring(0,5)}am`}
+          <div className="row lsn-row bg-dark bg-gradient pb-1 pt-md-3 pb-md-3 pb-lg-4">
+            <div className="text-white lsn-date pos-rel brd col-3 col-md-3 pt-md-3">
+              {`${((new Date(this.props.lsns.lsnTime)).toDateString()).substring(3,10)}`}
+              <div className="time-pos">
+                {`${((new Date(this.props.lsns.lsnTime)).toTimeString()).substring(0,5)}am`}
+              </div>
             </div>
-            </div>
-            <div className="push-r col-4 col-md-3 brd vert-c pt-md-3">
+            <div className="push-r brd vert-c col-4 col-md-3 pt-md-3" id="iName">
               <span className="bulk contrast">
-              {this.props.lsns.instructorName}
+                {this.props.lsns.instructorName}
               </span>
               {quantity > 0 &&
                 <div className="lesson-quantity-container " id="sphere">
@@ -36,15 +35,15 @@ class ListLesson extends Component {
                 </div>
               }
             </div>
-            <div className="push-r col-3 col-md-3 hot brd bulk type-pos vert-c pt-md-3">
+            <div className="push-r hot brd bulk type-pos vert-c col-3 col-md-3 pt-md-3">
               {this.props.lsns.lsnType}
             </div>
-            <div className="col-2 col-md-3 pl-2 pt-md-3 label-item brd text-white text-break pos-rel">
-              <div className="push-r mt-n2 mb-2 mb-md-0 lg-inline ms-1 ms-sm-2">
+            <div className="label-item brd text-white pos-rel col-2 col-md-3 pt-md-3 ">
+              <div className="push-r mb-2 mb-md-1 ms-1 ms-sm-2">
                 {this.props.lsns.lessonCost}
               </div>
-              <button className="btn btn-sm btn-warning px-sm-2 mt-md-0  px-md-3 pb-2 py-md-2 mr-n1 ml-sm-1 ms-sm-1 ml-md-5 d-block lg-inline cal-pos" onClick={() => {this.props.toggleAdd(); this.props.onLessonSelect(this.props.lsns)}}>
-                <FaRegCalendarCheck className="" id=""/>
+              <button className="btn btn-sm btn-warning cal-pos d-block text-black text-opacity-75 pb-2 py-md-2 px-sm-2 px-lg-3" onClick={() => {this.props.toggleAdd(); this.props.onLessonSelect(this.props.lsns)}}>
+                <FaRegCalendarCheck />
               </button>
             </div>
 
