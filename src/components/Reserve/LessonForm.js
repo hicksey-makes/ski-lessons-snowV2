01dd4ac1c7551b2a2cd74da41f8d00aa36f764bd
hicksey <hicksey14@gmail.com>
2023-05-18 14:39:23 -0600

    create sixth row in LessonForm.js to prevent instructor name/type word breaks

--- src/components/Reserve/LessonForm.js
+++ src/components/Reserve/LessonForm.js
@@ -12,8 +12,8 @@ function LessonForm(props) {
   return (
     <Container fluid>
       <Form onSubmit={(event) => {props.onFormSubmit(event); props.toggleAdd(); props.onLessonAdd(props.selectedLsn); }}>
-        <Row className="form-rows mt-5 mb-4 my-md-4">
-          <Col md={6} className="mb-4 ps-md-3 pe-md-0 ps-lg-4">
+        <Row className="form-rows mt-5 mb-4 my-md-5">
+          <Col md={6} className="mb-4  ps-md-3 pe-md-0 ps-lg-4">
             <Form.Group className="" controlId="formLessonFName">
                 <Form.Label>Name:</Form.Label>
                 <Form.Control autofocus className="form-name" type="text" required value={props.riderName}
@@ -27,60 +27,65 @@ function LessonForm(props) {
             </Form.Group>
           </Col>
         </Row>
-        <Row className="form-rows mb-3 mb-md-4">
-        <Col md={5} className="mb-5">
-          <Form.Group className="radio-group" key="lesson-length" controlId="formLessonRadio">
-            <Form.Label>Lesson Length:</Form.Label>
-            <div className="radios" id="formLessonRadio">
-              <Form.Check type="radio" className="no-vald-style fs-5" id="full-day-radio"  checked={props.lessonLength === "Full Day"} name="lesson length"
-              label="Full Day" value="Full Day" disabled={props.lessonLength === "Full Day"} onChange={event => props.setFullPrice(event)} />
+        <Row className="form-rows mb-3 mb-md-5">
+          <Col md={6} className="mb-5">
+            <Form.Group className="radio-group" key="lesson-length" controlId="formLessonRadio">
+              <Form.Label>Lesson Length:</Form.Label>
+              <div className="radios" id="formLessonRadio">
+                <Form.Check type="radio" className="no-vald-style fs-5" id="full-day-radio"  checked={props.lessonLength === "Full Day"} name="lesson length"
+                label="Full Day" value="Full Day" disabled={props.lessonLength === "Full Day"} onChange={event => props.setFullPrice(event)} />
 
-              <Form.Check type="radio" className="no-vald-style fs-5" id="half-day-radio" checked={props.lessonLength === "Half Day"} name="lesson length"
-              label="Half Day" value="Half Day" disabled={props.lessonLength === "Half Day"} onChange={event => props.setHalfPrice(event)} />
-            </div>
-          </Form.Group>
-        </Col>
-          <Col md={3} className="mb-5 mt-1">
+                <Form.Check type="radio" className="no-vald-style fs-5" id="half-day-radio" checked={props.lessonLength === "Half Day"} name="lesson length"
+                label="Half Day" value="Half Day" disabled={props.lessonLength === "Half Day"} onChange={event => props.setHalfPrice(event)} />
+              </div>
+            </Form.Group>
+          </Col>
+          <Col md={6} className="mb-5 mt-1">
             <div>
               <Form.Label>Instructor: </Form.Label>
               <span className="indigo fs-3 fst-italic text-opacity-75"> {props.selectedLsn.instructorName}</span>
             </div>
           </Col>
-          <Col md={4} className="mb-5">
+        </Row>
+        <Row className="mb-3 mb-md-5">
+          <Col md={{span: 5, offset: 1}} className="mb-5">
             <div className="">
               <Form.Label>Type: </Form.Label>
               <span className="indigo fs-3 fst-italic text-opacity-75"> {props.selectedLsn.lsnType}</span>
             </div>
           </Col>
-        </Row>
-        <Row className="form-rows mb-4 mb-md-4">
-          <Col md={4} className="mb-5">
-            <div className="form-r3 mx-md-auto">
+          <Col md={{span: 4, offset: 0}} className="mb-5">
+            <div className="form-r3 ">
               <Form.Label>Price: </Form.Label>
               <span className="indigo fs-4 fw-bold fst-italic text-opacity-75"> ${props.selectedLsn.lessonCost}</span>
             </div>
           </Col>
-          <Col md={4} className="mb-5">
-            <div className="form-r3 mx-md-auto">
+        </Row>
+        <Row className="form-rows mb-3 mb-md-4">
+          <Col md={{ span: 3, offset: 1}} className="mb-5">
+            <div className="form-r3 ">
               <Form.Label>Date:</Form.Label>{` `}
                 <mark className="py-2 px-1"><span className="fs-5 fw-bold text-danger">{((new Date(props.selectedLsn.lsnTime)).toDateString()).substring(3,10)}</span></mark>
             </div>
           </Col>
-          <Col md={4} className="mb-5">
-            <div className="form-r3 mx-md-auto">
+          <Col md={{ span: 4, offset: 2}} className="mb-5">
+            <div className="form-r3 ">
               <Form.Label >Time:</Form.Label>{` `}
                 <mark className="py-2 px-1"><span className="fs-5 fw-bold text-danger">{((new Date(props.selectedLsn.lsnTime)).toTimeString()).substring(0,5) + "am"}</span></mark>
             </div>
           </Col>
         </Row>
-        <Row className="form-rows mb-4 mb-md-4">
-          <Form.Group className="mb-3 form-row" key="" controlId="formLessonLNotes">
-            <Form.Label >Lesson Notes:</Form.Label>
-            <Form.Control as="textarea" value={props.lessonNotes} onChange={event => props.onNotesChange(event)} rows={5} placeholder="Lesson Notes" />
-          </Form.Group>
+        <Row className="form-rows mb-2 mb-md-4">
+          <Col md={{ span: 10, offset: 2}} className="">
+            <Form.Group className="mb-5 form-row" key="" controlId="formLessonLNotes">
+              <Form.Label >Lesson Notes:</Form.Label>
+              <Form.Control as="textarea" value={props.lessonNotes} onChange={event => props.onNotesChange(event)} rows={5} placeholder="Lesson Notes" />
+            </Form.Group>
+          </Col>
+
         </Row>
         <div className="form-r5">
-            <Row className="justify-content-end my-4 my-md-3">
+            <Row className="justify-content-end mb-4 mb-md-3">
               <Col sm={{ span: 4, offset: 8 }} md={{ span: 3, offset: 9 }}>
                 <Button type="submit" className="btn-lg">Reserve</Button>
               </Col>
